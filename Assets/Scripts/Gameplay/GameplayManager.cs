1e1d920889c7da4e6923cbd47e87d388d4b98d86
Adrian Nieściur <63786641+factuall@users.noreply.github.com>
2023-10-11 21:33:52 +0200

    minor touch-ups and setup for further testing/developing

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -12,7 +12,7 @@ public class GameplayManager : MonoBehaviour
     bool gameplayReady = false;
 
     /*******FLOOR GENERATION VARIABLES*******/
-    public static float ROOM_WIDTH = 30, ROOM_HEIGHT = 16.5f;
+    public static float ROOM_WIDTH = 16, ROOM_HEIGHT = 9;
     public static int floorSize = 20;
     public int connectiorRoomCap = 3;
     public int roomCap = 20;
@@ -24,13 +24,13 @@ public class GameplayManager : MonoBehaviour
     void Start()
     {
         mainCamera = Camera.main;
-        GenerateFloor();
-        currentRoom = floorRooms[10, 10];
-        gameplayReady = true;
-        GetComponentInChildren<PlayerController>().transform.position = new Vector3(
-            currentRoom.roomInstance.transform.position.x,
-            currentRoom.roomInstance.transform.position.y,
-            0);
+        //GenerateFloor();
+        //currentRoom = floorRooms[10, 10];
+        //gameplayReady = true;
+        //GetComponentInChildren<PlayerController>().transform.position = new Vector3(
+        //    currentRoom.roomInstance.transform.position.x,
+        //    currentRoom.roomInstance.transform.position.y,
+        //    0);
 
     }
 
@@ -48,6 +48,28 @@ public class GameplayManager : MonoBehaviour
             else if (Input.GetKeyDown(KeyCode.LeftArrow)) currentRoom = floorRooms[currentRoom.X - 1, currentRoom.Y];
             if (Input.GetKeyDown(KeyCode.UpArrow)) currentRoom = floorRooms[currentRoom.X, currentRoom.Y + 1];
             else if (Input.GetKeyDown(KeyCode.DownArrow)) currentRoom = floorRooms[currentRoom.X, currentRoom.Y - 1];
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            for (int lY = 0; lY < floorRooms.GetLength(1); lY++)
+            {
+                for (int lX = 0; lX < floorRooms.GetLength(0); lX++)
+                {
+                    if (floorRooms[lX, lY] != null) 
+                    if (floorRooms[lX, lY].isOccupied()) Destroy(floorRooms[lX, lY].roomInstance);
+                    floorRooms[lX, lY] = new GameRoom(lX, lY);
+                }
+            }
+            //GenerateFloor();
+            GenerateFloor();
+            currentRoom = floorRooms[10, 10];
+            gameplayReady = true;
+            GetComponentInChildren<PlayerController>().transform.position = new Vector3(
+                currentRoom.roomInstance.transform.position.x,
+                currentRoom.roomInstance.transform.position.y,
+                0);
         }
     }
 
@@ -83,12 +105,15 @@ public class GameplayManager : MonoBehaviour
 
         while (roomCount < roomCap)
         {
+            bool generatedAnything = false;
             for (int lY = 0; lY < floorRooms.GetLength(1); lY++)
             {
                 for (int lX = 0; lX < floorRooms.GetLength(0); lX++)
                 {
                     // skip if the room is occupied 
+                    
                     if (!floorRooms[lX, lY].isOccupied()) continue;
+                    if (floorRooms[lX, lY].generatedNeighbors) continue;
                     // for all neighbors of the room
                     foreach (GameRoom neighbor in GetNeighbors(floorRooms[lX, lY]))
                     {
@@ -112,10 +137,12 @@ public class GameplayManager : MonoBehaviour
                         // create the room
                         floorRooms[neighbor.X, neighbor.Y].roomInstance = Instantiate(roomPrefab, new Vector3(neighbor.X * ROOM_WIDTH, neighbor.Y * ROOM_HEIGHT, 0), transform.rotation, transform);
                         roomCount++;
+                        generatedAnything = true;
                     }
-
+                    floorRooms[lX, lY].generatedNeighbors = true; 
                 }
             }
+            if (!generatedAnything) break;
         }
     }
 }
@@ -124,6 +151,7 @@ public class GameRoom
 {
     public int X = 0, Y = 0;
     public GameObject roomInstance = null;
+    public bool generatedNeighbors = false;
     public bool isOccupied()
     {
         return roomInstance != null;
